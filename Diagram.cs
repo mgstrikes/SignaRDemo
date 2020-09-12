17b5aa0bc60787d33b90134e1f3b94202f6dc97f
mgstrikes <manugeorgeks@gmail.com>
2020-09-12 22:15:09 +0100

    working copy

---
+++ Diagram.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Microsoft.Azure.SignalR.Samples.Whiteboard
+{
+    public class Diagram
+    {
+        public class Shape
+        {
+            public string Kind { get; set; }
+
+            public string Color { get; set; }
+
+            public int Width { get; set; }
+
+            public List<int> Data { get; set; }
+        }
+
+        private int totalUsers = 0;
+
+        public byte[] Background { get; set; }
+
+        public string BackgroundContentType { get; set; }
+
+        public string BackgroundId { get; set; }
+
+        public ConcurrentDictionary<string, Shape> Shapes { get; } = new ConcurrentDictionary<string, Shape>();
+
+        public int UserEnter()
+        {
+            return Interlocked.Increment(ref totalUsers);
+        }
+
+        public int UserLeave()
+        {
+            return Interlocked.Decrement(ref totalUsers);
+        }
+    }
+}
---
+++ Hub/DrawHub.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+
+namespace Microsoft.Azure.SignalR.Samples.Whiteboard
+{
+    public class DrawHub : Hub
+    {
+        private Diagram diagram;
+
+        public DrawHub(Diagram diagram)
+        {
+            this.diagram = diagram;
+        }
+        public override Task OnConnectedAsync()
+        {
+            var t = Task.WhenAll(diagram.Shapes.AsEnumerable().Select(l => Clients.Client(Context.ConnectionId).SendAsync("ShapeUpdated", l.Key, l.Value)));
+            if (diagram.Background != null) t = t.ContinueWith(_ => Clients.Client(Context.ConnectionId).SendAsync("BackgroundUpdated", diagram.BackgroundId));
+            return t.ContinueWith(_ => Clients.All.SendAsync("UserUpdated", diagram.UserEnter()));
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return Clients.All.SendAsync("UserUpdated", diagram.UserLeave());
+        }
+
+        public async Task PatchShape(string id, List<int> data)
+        {
+            diagram.Shapes[id].Data.AddRange(data);
+            await Clients.Others.SendAsync("ShapePatched", id, data);
+        }
+
+        public async Task UpdateShape(string id, Diagram.Shape shape)
+        {
+            diagram.Shapes[id] = shape;
+            await Clients.Others.SendAsync("ShapeUpdated", id, shape);
+        }
+
+        public async Task RemoveShape(string id)
+        {
+            diagram.Shapes.Remove(id, out _);
+            await Clients.Others.SendAsync("ShapeRemoved", id);
+        }
+
+        public async Task Clear()
+        {
+            diagram.Shapes.Clear();
+            diagram.Background = null;
+            await Clients.Others.SendAsync("Clear");
+        }
+
+        public async Task SendMessage(string name, string message)
+        {
+            await Clients.Others.SendAsync("NewMessage", name, message);
+        }
+    }
+}
